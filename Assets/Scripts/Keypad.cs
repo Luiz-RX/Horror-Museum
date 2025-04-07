ed0d0855ea22f75da7ed65dcc5656fdd369c8f0a
MarcoGM7 <124256964+MarcoGM7@users.noreply.github.com>
2025-04-07 17:43:17 +0200

    Keycode básico terminado

--- Assets/Scripts/Keypad.cs
+++ Assets/Scripts/Keypad.cs
@@ -4,10 +4,36 @@ using TMPro;
 
 public class Keypad : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI answer;
+    [SerializeField] TextMeshProUGUI enteredNumbers;
+    private string answer = "1234";
     void Start()
     {
         
+    }
+
+    public void setNum(int num)
+    {
+        if (enteredNumbers.textInfo.characterCount < 4)
+        {
+            enteredNumbers.text += num.ToString();
+        }
+    }
+
+    public void Execute()
+    {
+        if (enteredNumbers.text == answer)
+        {
+            //Abrir Puerta
+            Debug.Log("SI");
+        } else
+        {
+            Debug.Log("Error");
+        }
+    }
+
+    public void Clear()
+    {
+        enteredNumbers.text = "";
     }
 
     // Update is called once per frame

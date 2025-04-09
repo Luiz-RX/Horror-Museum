523536838c71d3d20c9ecb6fbec6d6f5484fcbd7
MarcoGM7 <124256964+MarcoGM7@users.noreply.github.com>
2025-04-09 14:19:19 +0200

    Script de cambio de camara hecho

--- Assets/Scripts/CameraZoom.cs
+++ Assets/Scripts/CameraZoom.cs
@@ -1,21 +1,77 @@
 using UnityEngine;
+using Unity.Cinemachine;
+
 
 public class CameraZoom : MonoBehaviour
 {
+    public CinemachineCamera playerCam;
+    public CinemachineCamera interactCam;
+
+    public Collider[] buttonColliders;
+
+    int activeCam = 1;
+
+    bool canInteract;
     
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if(canInteract)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                //Change cam and enable buttons
+                ChangeCam();
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        
+        canInteract = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        canInteract = false;
+    }
+
+    void ChangeCam() 
+    { 
+        if (activeCam == 1)
+        {
+            playerCam.enabled = false;
+            interactCam.enabled = true;
+            EnableButtons();
+            activeCam = 2;
+
+        } else if(activeCam == 2)
+        {
+            interactCam.enabled = false;
+            playerCam.enabled = true;
+            DisableButtons();
+            activeCam = 1;
+        }
+    }
+
+    void EnableButtons()
+    {
+        for (int i = 0; i < buttonColliders.Length; i++)
+        {
+            buttonColliders[i].enabled = true;
+        }
+    }
+
+    void DisableButtons()
+    {
+        for (int i = 0; i < buttonColliders.Length; i++)
+        {
+            buttonColliders[i].enabled = false;
+        }
     }
 }

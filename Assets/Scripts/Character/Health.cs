9a3d6dc70df1ff17456002a3f5059a2463e82fa5
Luiz-RX <22295673@live.uem.es>
2025-05-15 04:03:04 +0200

    Muerte personaje

--- Assets/Scripts/Character/Health.cs
+++ Assets/Scripts/Character/Health.cs
@@ -3,12 +3,22 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] private int health = 6;
+    private Animator animator;
+    private CharacterController characterController;
 
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
+        characterController.enabled = true;
+        animator.SetBool("Die", false);
+    }
     public void TakeDamage(int damage)
     {
         if (health <= 0) 
         {
-            //Morirse
+            animator.SetBool("Die", true);
+            characterController.enabled = false;
         }
         health -= damage;
     }

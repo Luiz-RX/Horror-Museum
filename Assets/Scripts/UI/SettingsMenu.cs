c75e91cde5a97c47f4cac8bb52e0e67e147a7972
Luiz-RX <luisip2001@gmail.com>
2025-10-17 21:13:26 +0200

    Viñeta shader añadida, arreglo de menu

--- Assets/Scripts/UI/SettingsMenu.cs
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -11,7 +11,7 @@ public class SettingsMenu : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Time.timeScale = 1.0f;
     }
 
     // Update is called once per frame
